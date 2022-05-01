63bcab27eb1182554737a86a1af38681d71e646b
yvahosc <yvahosc@unal.edu.co>
2022-05-01 10:28:43 -0500

    Creación proyecto ejercicio 2

---
+++ Ejercicio2/src/Main/AnotherMainClass.java
@@ -0,0 +1,46 @@
+package Main;
+
+import Classes.CoachStaff;
+import Classes.Players;
+
+/**
+ * [ Esta clase tiene como objetivo iniciar la aplicación en la cual se 
+ * podrá realizar la visualización de los objetos pertenecientes a la selección
+ * nacional de futbol, jugadores y cuerpo técnico y sus atributos.
+ * ]
+ *
+ * @version ["VS" 1.0.0,2022-04-30
+ *
+ * 1.0.0,2022-04-30, La clase corresponde a la versión 1.0.0 del sistema,
+ * la misma hasta el momento no ha recibido ningun refactorings ]
+ *
+ * @author [Yeisson Augusto Vahos Cortes, yvahosc@unal.edu.co]
+ * 
+ * @since [1.0.0]
+ *
+ */
+public class AnotherMainClass {
+
+    /**
+     * [El objetivo principal de este método es iniciar la ejecución del 
+     * programa en el que se podrá realizar la visualización de los objetos 
+     * pertenecientes a la selección nacional de futbol, jugadores y cuerpo 
+     * técnico y sus atributos, llamando a los método main que se encuentran
+     * en cada una de las clases (jugadores y cuerpo técnico).
+     * ]
+     *
+     * @param args
+     *
+     * @author [Yeisson Augusto Vahos Cortes, yvahosc@unal.edu.co]
+     *      
+     * @since [1.0.0]
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Imprimiendo desde metodo main en clase "
+                + "otra clase principal.\n");
+        
+        Players.main(args);
+        CoachStaff.main(args); 
+    }  
+}
